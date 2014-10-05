9ae79b3e5b2bb7857680251d8596174cfa305b4f
Jamie White <jamie@jgwhite.co.uk>
2014-10-05 19:12:57 +0200

    Add test

--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,7 +1,28 @@
 defmodule PortalTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "pushing left and right" do
+    Portal.shoot(:a)
+    Portal.shoot(:b)
+
+    portal = Portal.transfer(:a, :b, [1, 2, 3, 4])
+
+    assert Portal.Door.get(:a) == [4, 3, 2, 1]
+    assert Portal.Door.get(:b) == []
+
+    Portal.push_right(portal)
+
+    assert Portal.Door.get(:a) == [3, 2, 1]
+    assert Portal.Door.get(:b) == [4]
+
+    Portal.push_right(portal)
+
+    assert Portal.Door.get(:a) == [2, 1]
+    assert Portal.Door.get(:b) == [3, 4]
+
+    Portal.push_left(portal)
+
+    assert Portal.Door.get(:a) == [3, 2, 1]
+    assert Portal.Door.get(:b) == [4]
   end
 end
